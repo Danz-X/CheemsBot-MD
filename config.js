1f9476bf92259bcec83fde36104c1aca9c07fe5f
Danz-X <74555583+Danz-X@users.noreply.github.com>
2022-04-04 07:24:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['6281252009563','6281252009563'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['6281252009563'] //another owner number
+global.premium = ['6281252009563'] //premium number
+global.pengguna = 'Raditya' //username
+global.botnma = 'Danzz Bot-MD' //bot name
+global.ownernma = 'Raditya' //owner name
+global.packname = 'Danzz Bot' //sticker package name
+global.author = 'By Raditya' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
